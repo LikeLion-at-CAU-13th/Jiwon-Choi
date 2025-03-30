d7adefd8dd2df3508685520a7d0242d1904c9004
ji-circle <sp202401jw@gmail.com>
2025-03-30 22:13:03 +0900

    feat: comment, category

--- django02/posts/models.py
+++ django02/posts/models.py
@@ -1,38 +1,4 @@
-# from django.db import models
-# from accounts.models import User
-
-# # Create your models here.
-# class Post(models.Model):
-
-#     #둘 중 하나를 고르는 것
-#     # status 부분에서, 게시글의 상태를 나타낼 때 사용 
-#     CHOICES = (
-#         ('STORED', '보관'),
-#         ('PUBLISHED', '발행')
-#     )
-
-#     #id는 autofield로 함
-#     id = models.AutoField(primary_key=True)
-#     # max_length 지정
-#     title = models.CharField(max_length=30)
-#     # max length 없는...? @@
-#     content = models.TextField()
-#     #status -> default를 store로 설정함
-#     status = models.CharField(max_length=15, choices=CHOICES, default='STORED')
-#     created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜와 시간 저장
-#     updated = models.DateTimeField(auto_now=True)  # 객체를 저장할 때 날짜와 시간 갱신
-#     #외래키
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-
-#     # title을 리턴하게 됨.
-#     def __str__(self): # 표준 파이썬 클래스 메서드, 사람이 읽을 수 있는 문자열을 반환하도록 함
-#         return self.title
-    
-
-
-
-
-    #baseModel 활용하는 버전
+#baseModel 활용하는 버전
 from django.db import models
 from accounts.models import User
 
@@ -45,7 +11,6 @@ class BaseModel(models.Model): # models.Model을 상속받음
     #추상 클래스를 의미한다
     class Meta:
         abstract = True
-
 
 class Post(BaseModel): # BaseModel을 상속받음
 
@@ -64,3 +29,28 @@ class Post(BaseModel): # BaseModel을 상속받음
     # 수정하면 내용도 보임
     def __str__(self):
         return self.title
+    
+class Category(models.Model):
+    cat_id = models.AutoField(primary_key=True)
+    cat_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.cat_name
+
+class PostCategory(models.Model):
+    cat_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.post_id.title} - {self.cat_id.cat_name}'
+    
+class Comment(BaseModel):
+    comment_id = models.AutoField(primary_key=True)
+    post_id = models.ForeignKey(Post, on_delete= models.CASCADE, related_name='comment')
+    comment_name = models.CharField(max_length=100)
+    comment_content = models.TextField()
+
+    def __str__(self):
+        return self.comment_content
+
+
