c5b08b00b35c8d8cce27af2b71d6efefadc68d6d
ji-circle <sp202401jw@gmail.com>
2025-04-06 22:03:00 +0900

    feat: create

--- django02/posts/urls.py
+++ django02/posts/urls.py
@@ -3,13 +3,16 @@ from posts.views import *
 
 urlpatterns = [
     #경로를 지정하지 않은 것(공백)이 django_review 함수를 바라보게 설정
-    path('', django_review, name = 'django_review'),
+    # path('', django_review, name = 'django_review'),
     #예를 들어, 'books' 경로로 접근하면 book_list를 호출하려면
     #path("books/", book_list),
-    path('page', index, name='my-page'),
+    # path('page', index, name='my-page'),
 
     # 결과를 반환받기 위해 url 추가?
     # int:id는 변화할 수 있는 값들. 1번을 넣으면 1번에 해당하는 게시글이 반환되게 하는 역할을 함
     
-    path('<int:id>', get_post_detail)
+    # path('<int:id>', get_post_detail)
+
+    path('', post_list, name="post_list"),
+    
 ]
--- django02/posts/views.py
+++ django02/posts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse # 데이터를 JSON에 담기 위해서 사용
 from django.shortcuts import get_object_or_404 # 추가
 
-#추가4개
-from django.views.decorators.http import require_http_methods # 추가
-from .models import * # 추가
+#4주? 추가4개
+from django.views.decorators.http import require_http_methods # 4주? 추가
+from .models import * # 4주? 추가
+
+# 5주 추가
+import json
 
 # Create your views here.
 
@@ -19,7 +22,7 @@ def index(request):
     return render(request, 'index.html')
 
 
-#추가한 부분들
+#4주에 추가한 부분들
 
 # Create your views here.
 # GET을 허용해서... @
@@ -40,3 +43,44 @@ def get_post_detail(reqeust, id):
         "data": post_detail_json})
 
 #http://127.0.0.1:8000/1 postman에서 확인하기
+
+# 5주 추가
+import json
+
+# 함수 데코레이터, 특정 http method만 허용
+@require_http_methods(["POST"])
+def post_list(request):
+    
+    if request.method == "POST":
+    
+        # byte -> 문자열 -> python 딕셔너리
+        body = json.loads(request.body.decode('utf-8'))
+    
+		    # 프론트에게서 user id를 넘겨받는다고 가정.
+		    # 외래키 필드의 경우, 객체 자체를 전달해줘야하기 때문에
+        # id를 기반으로 user 객체를 조회해서 가져옵니다 !
+        user_id = body.get('user')
+        user = get_object_or_404(User, pk=user_id)
+
+	    # 새로운 데이터를 DB에 생성
+        new_post = Post.objects.create(
+            title = body['title'],
+            content = body['content'],
+            status = body['status'],
+            user = user
+        )
+    
+	    # Json 형태 반환 데이터 생성
+        new_post_json = {
+            "id": new_post.id,
+            "title" : new_post.title,
+            "content": new_post.content,
+            "status": new_post.status,
+            "user": new_post.user.id
+        }
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 생성 성공',
+            'data': new_post_json
+        })
