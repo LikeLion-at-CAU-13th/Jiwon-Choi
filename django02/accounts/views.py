30f3ae4574d0b7fd57c949bddd9cb06026a3d794
ji-circle <sp202401jw@gmail.com>
2025-05-28 19:54:41 +0900

    feat: session

--- django02/accounts/views.py
+++ django02/accounts/views.py
@@ -78,3 +78,85 @@ class LogoutView(APIView):
     def post(self, request):
         logout(request)
         return Response({"message": "logout success!"}, status=status.HTTP_200_OK)
+
+#11주차 OAuth 추가 아래로   
+from config.settings import get_secret
+
+# 구글 소셜로그인
+GOOGLE_REDIRECT = get_secret("GOOGLE_REDIRECT")
+GOOGLE_CALLBACK_URI = get_secret("GOOGLE_CALLBACK_URI")
+GOOGLE_CLIENT_ID = get_secret("GOOGLE_CLIENT_ID")
+GOOGLE_SECRET = get_secret("GOOGLE_SECRET")
+GOOGLE_SCOPE = get_secret("GOOGLE_SCOPE")
+
+from django.shortcuts import redirect
+from json import JSONDecodeError
+from django.http import JsonResponse
+import requests 
+
+def google_login(request):
+    return redirect(f"{GOOGLE_REDIRECT}?client_id={GOOGLE_CLIENT_ID}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={GOOGLE_SCOPE}")
+    
+# 11주차 - 윗부분과 이어서!  인가코드로 access token 요청부분~  
+# from rest_framework import status
+from .serializers import OAuthSerializer
+
+def google_callback(request):
+    # 인가코드 받아오기
+    code = request.GET.get("code", None)     
+    
+    if code is None:
+        return JsonResponse({'error': 'Authorization code error.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # 인가코드로 access token 받기
+    token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={GOOGLE_CLIENT_ID}&client_secret={GOOGLE_SECRET}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}")
+    token_req_json = token_req.json()
+    error = token_req_json.get("error")
+
+    if error is not None:
+        raise JSONDecodeError(error)
+
+    google_access_token = token_req_json.get('access_token')
+
+    # access token으로 구글 계정 정보 가져오기
+    user_info = requests.get(f"https://www.googleapis.com/oauth2/v3/userinfo?access_token={google_access_token}")
+    res_status = user_info.status_code
+
+    # 에러처리...?
+    if res_status != 200:
+        return JsonResponse({'status': 400,'message': 'Failed to get access token'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user_info_json = user_info.json()
+
+    data = {
+        "username": user_info_json['name'],
+        "email": user_info_json['email']
+    }
+    
+    # serializer에 데이터 값 넣어줌
+    serializer = OAuthSerializer(data=data)
+    # validated_data는 위에서 만든 대로... @@ 추가하기
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.validated_data["user"]
+        access_token = serializer.validated_data["access_token"]
+        refresh_token = serializer.validated_data["refresh_token"]
+
+        res = JsonResponse(
+            {
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                },
+                "message": "google social login success!",
+                "token": {
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                },
+            },
+            status=status.HTTP_200_OK,
+        )
+        # 쿠키값에 토큰 넣어줌
+        res.set_cookie("access-token", access_token, httponly=True)
+        res.set_cookie("refresh-token", refresh_token, httponly=True)
+        return res
